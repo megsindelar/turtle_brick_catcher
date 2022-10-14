5e575e1cd5370cf31efa4a7884eb8aa7a9a91ef6
Meg <megansindelar2023@u.northwestern.edu>
2022-10-13 20:26:38 -0500

    wall marker is working!!

--- turtle_brick/launch/turtle_arena.launch.py
+++ turtle_brick/launch/turtle_arena.launch.py
@@ -12,25 +12,23 @@ from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
-    rviz_path = get_package_share_path('turtle_brick')
-    default_rviz_config_path = rviz_path / 'rvizconfig.rviz'
+    # rviz_path = get_package_share_path('turtle_brick')
+    # default_rviz_config_path = rviz_path / 'rvizconfig.rviz'
 
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
-                                     description='Absolute path to rviz config file')
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
-    )
+    # rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
+    #                                  description='Absolute path to rviz config file')
+    # rviz_node = Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     name='rviz2',
+    #     output='screen',
+    #     arguments=['-d', LaunchConfiguration('rvizconfig')],
+    # )
 
     arena_node = Node(package='turtle_brick',
         executable='arena'
     )
 
     return LaunchDescription([
-        rviz_arg,
-        rviz_node,
-        arena_node,
+        arena_node
     ])
--- turtle_brick/turtle_brick/arena.py
+++ turtle_brick/turtle_brick/arena.py
@@ -1,9 +1,13 @@
+from audioop import add
+from distutils.log import info
 from matplotlib.pyplot import cla
+from numpy import block
 import rclpy
 from rclpy.node import Node
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 from tf2_ros import TransformBroadcaster
-from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import TransformStamped, Point
+from visualization_msgs.msg import Marker, MarkerArray
 from sensor_msgs.msg import JointState
 from .quaternion import angle_axis_to_quaternion
 
@@ -18,8 +22,91 @@ class Arena(Node):
     def __init__(self):
         super().__init__('arena')
 
+        #create a broadcaster that will publish to /tf_static
+        self.static_broadcaster = TransformBroadcaster(self)
+
         #create a broadcaster that will repeatedly publish to /tf
-        #self.broadcaster = TransformBroadcaster(self)
+        self.broadcaster = TransformBroadcaster(self)
+
+        #create marker publisher
+        self.pub_marker = self.create_publisher(MarkerArray,"visualization_marker_array",10)
+
+        #marker
+        self.wall1 = Marker()
+        self.wall1.header.frame_id = "/world"
+        self.wall1.header.stamp = self.get_clock().now().to_msg()
+        self.wall1.id = 1
+        self.wall1.type = self.wall1.CUBE
+        self.wall1.action = self.wall1.ADD
+        self.wall1.scale.x = 10.0
+        self.wall1.scale.y = 0.1
+        self.wall1.scale.z = 0.5
+        self.wall1.color.r = 1.0
+        self.wall1.color.g = 0.0
+        self.wall1.color.b = 1.0
+        self.wall1.color.a = 1.0
+        self.wall1.pose.position.x = 0.0
+        self.wall1.pose.position.y = -5.0
+        self.wall1.pose.position.z = 0.0
+     
+        self.wall2 = Marker()
+        self.wall2.header.frame_id = "/world"
+        self.wall2.header.stamp = self.get_clock().now().to_msg()
+        self.wall2.id = 2
+        self.wall2.type = self.wall2.CUBE
+        self.wall2.action = self.wall2.ADD
+        self.wall2.scale.x = 10.0
+        self.wall2.scale.y = 0.1
+        self.wall2.scale.z = 0.5
+        self.wall2.color.r = 1.0
+        self.wall2.color.g = 0.0
+        self.wall2.color.b = 1.0
+        self.wall2.color.a = 1.0
+        self.wall2.pose.position.x = 0.0
+        self.wall2.pose.position.y = 5.0
+        self.wall2.pose.position.z = 0.0
+
+        self.wall3 = Marker()
+        self.wall3.header.frame_id = "/world"
+        self.wall3.header.stamp = self.get_clock().now().to_msg()
+        self.wall3.id = 3
+        self.wall3.type = self.wall3.CUBE
+        self.wall3.action = self.wall3.ADD
+        self.wall3.scale.x = 0.1
+        self.wall3.scale.y = 10.0
+        self.wall3.scale.z = 0.5
+        self.wall3.color.r = 1.0
+        self.wall3.color.g = 0.0
+        self.wall3.color.b = 1.0
+        self.wall3.color.a = 1.0
+        self.wall3.pose.position.x = 5.0
+        self.wall3.pose.position.y = 0.0
+        self.wall3.pose.position.z = 0.0
+
+        self.wall4 = Marker()
+        self.wall4.header.frame_id = "/world"
+        self.wall4.header.stamp = self.get_clock().now().to_msg()
+        self.wall4.id = 4
+        self.wall4.type = self.wall4.CUBE
+        self.wall4.action = self.wall4.ADD
+        self.wall4.scale.x = 0.1
+        self.wall4.scale.y = 10.0
+        self.wall4.scale.z = 0.5
+        self.wall4.color.r = 1.0
+        self.wall4.color.g = 0.0
+        self.wall4.color.b = 1.0
+        self.wall4.color.a = 1.0
+        self.wall4.pose.position.x = -5.0
+        self.wall4.pose.position.y = 0.0
+        self.wall4.pose.position.z = 0.0
+
+
+        self.wall_array = MarkerArray()
+        self.wall_array.markers.append(self.wall1)
+        self.wall_array.markers.append(self.wall2)
+        self.wall_array.markers.append(self.wall3)
+        self.wall_array.markers.append(self.wall4)
+  
 
         #create a timer callback to broadcast transforms at 100 Hz
         self.timer = self.create_timer(0.01, self.timer)
@@ -28,17 +115,17 @@ class Arena(Node):
 
     def timer(self):
 
-        # world__brick_link = TransformStamped()
-        x = self.dx
-        #time = self.get_clock().now().to_msg()
-        # world__brick_link.header.frame_id = "world"
-        # world__brick_link.child_frame_id = "brick"
-        # world__brick_link.header.stamp = time
-        # world__brick_link.transform.translation.z = float(self.dx)
+        odom__brick_link = TransformStamped()
+        time = self.get_clock().now().to_msg()
+        odom__brick_link.header.frame_id = "odom"
+        odom__brick_link.child_frame_id = "brick"
+        odom__brick_link.header.stamp = time
+        odom__brick_link.transform.translation.z = float(self.dx)
 
-        # self.broadcaster.sendTransform(world__brick_link)
+        self.broadcaster.sendTransform(odom__brick_link)
 
-
+        self.pub_marker.publish(self.wall_array)
+        
 def arena_entry(args=None):
     rclpy.init(args=args)
     arena = Arena()
